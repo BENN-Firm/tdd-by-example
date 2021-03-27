6d783d6c119fb2ce9b310198320f7c1396cce558
Nabil <nabil.bensrhier@firmenich.com>
2021-03-27 16:51:13 +0100

    Assertions + Assumptions

--- src/test/java/guru/testing/model/PersonTest.java
+++ src/test/java/guru/testing/model/PersonTest.java
@@ -1,24 +1,18 @@
 package guru.testing.model;
 
-<<<<<<< HEAD
-import org.junit.jupiter.api.Disabled;
+
 import org.junit.jupiter.api.DisplayName;
-=======
->>>>>>> origin/assertions
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-<<<<<<< HEAD
+
 //@Disabled(value = "Disable until we learn mocking!!")
 class PersonTest {
 
     //@Disabled(value = "Disable until we learn mocking!!")
     @DisplayName("Person test properties")
-=======
-class PersonTest {
-
->>>>>>> origin/assertions
     @Test
     void groupAssertions(){
         //given
@@ -30,10 +24,7 @@ class PersonTest {
                 () -> assertEquals(person.getLastName(), "Bensrhier"));
     }
 
-<<<<<<< HEAD
-    @DisplayName("Person test properties 2")
-=======
->>>>>>> origin/assertions
+
     @Test
     void groupAssertions2(){
         //given
